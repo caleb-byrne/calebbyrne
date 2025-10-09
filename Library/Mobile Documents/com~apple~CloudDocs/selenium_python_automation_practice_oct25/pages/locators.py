3213faff055484388553945be88e318966df7ccf
caleb byrne <calebmbyrne@gmail.com>
2025-10-09 15:17:36 -0700

    chore: remove unused pages/locators.py

--- Library/Mobile Documents/com~apple~CloudDocs/selenium_python_automation_practice_oct25/pages/locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginLocators:
-    USERNAME = (By.ID, "user-name")
-    PASSWORD = (By.ID, "password")
-    LOGIN_BUTTON = (By.ID, "login-button")
